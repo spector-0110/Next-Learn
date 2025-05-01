ffa92f605f77ebd3691e58f0dc4eb3573945153b
spector-0110 <vatsaadityamishra19@gmail.com>
2025-05-01 20:53:56 +0530

    first

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+  return (
+     <p className="text-gray-500">Customers page</p>
+  );
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return (
+        <p> this is invoices</p>
+    );
+};
